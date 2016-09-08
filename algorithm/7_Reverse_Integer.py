9b1dc0fe7d6c3ec68224d6c96a6fa8c00325e5dd
Evan Chang <chin.yeng@gmail.com>
2016-09-08 11:04:40 +0800

    Create 7_Reverse_Integer.py

---
+++ algorithm/7_Reverse_Integer.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x >= 0:
+            sign = 1
+        else:
+            sign = -1
+            x = abs(x)
+
+        ans = 0
+        while x:
+            ans = ans * 10 + x % 10
+            x /= 10
+
+        reverse_out = sign *ans
+
+        if  ((reverse_out > 2147483647)or(reverse_out < -2147483646)): # handle 32-bit overflow            
+            return 0
+        else:
+            return reverse_out
+
+t = Solution()
+#print t.reverse(1534236469)
+print t.reverse(1)
