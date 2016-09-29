a01c837c46aff1184c67f47a0d76a02845af3634
Evan Chang <chin.yeng@gmail.com>
2016-09-29 16:09:07 +0800

    Create 225_Implement_Stack_using_Queues.py

---
+++ algorithm/225_Implement_Stack_using_Queues.py
@@ -0,0 +1,33 @@
+class Stack(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.data = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: nothing
+        """
+        self.data.append(x)
+        
+
+    def pop(self):
+        """
+        :rtype: nothing
+        """
+        self.data.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.data[-1]
+        
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return not self.data
