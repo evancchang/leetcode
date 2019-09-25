a4abc06f06d694f6b3eeb29f9f78e010f8ef0adc
Evan Chang <chin.yeng@gmail.com>
2019-09-25 22:00:43 +0800

    Update 14_Longest_Common_Prefix.py

--- algorithm/14_Longest_Common_Prefix.py
+++ algorithm/14_Longest_Common_Prefix.py
@@ -4,41 +4,18 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
+        res = ""
         if not strs:
-            return ""
-
-        min_length = len(strs[0])
-        prefix = strs[0]
-        if not min_length:
-            return ""
-
-        min_index = 0
-        for i in xrange(len(strs)):
-            if min_length > len(strs[i]):
-                min_length = len(strs[i])
-                min_index = i
-                prefix = strs[i]
-
-        if len(strs) < 2:
-            return prefix
-        else:
-            strs.remove(prefix)
-
-        idx = -1
-        compare = True
-
-        for i in xrange(len(prefix)):
-            for word in strs:
-                if compare:
-                    if prefix[i] != word[i]:
-                        compare = False
-            if compare: # record the prefix index
-                idx = i
-
-        if idx != -1:
-            return prefix[0:idx+1]
-        else:
-            return ""
+            return res
+        
+        strs.sort()
+        for i in range(len(strs[0])):
+            if strs[0][i] != strs[-1][i]:
+                break
+            else:
+                res += strs[0][i]
+                
+        return res
 
 t = Solution()
 #print t.longestCommonPrefix(['awbc', 'ab', 'aebcde'])
