4116a4aecfe8c8602c1a0c2e4a5eb15be0026a47
Evan Chang <chin.yeng@gmail.com>
2019-10-01 22:02:22 +0800

    Update 26_remove_dup_sorted_array.py

--- algorithm/26_remove_dup_sorted_array.py
+++ algorithm/26_remove_dup_sorted_array.py
@@ -4,18 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        length = len(nums)
-        if length:
-            idx = 1
-            for i in range(1, length):
-                if nums[i] != nums[i-1]:
-                    nums[idx] = nums[i]
-                    idx += 1
-
-            #nums = nums[:idx]
-            return idx
-        else:
-            return length
+        count = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[count]:
+                count += 1
+                nums[count] = nums[i]
+        
+        return count + 1
 
 t = Solution()
 print t.removeDuplicates([1,1,2,2,3])
