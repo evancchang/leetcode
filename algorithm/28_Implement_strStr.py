ac281ad4dfb49eab0be647f60577dfeaf7a60f13
Evan Chang <chin.yeng@gmail.com>
2019-10-02 21:47:58 +0800

    Update 28_Implement_strStr.py

--- algorithm/28_Implement_strStr.py
+++ algorithm/28_Implement_strStr.py
@@ -1,20 +1,22 @@
 class Solution(object):
     def strStr(self, haystack, needle):
-		"""
-		:type haystack: str
-		:type needle: str
-		:rtype: int
-		"""
-		if (not haystack) and (not needle):
-			return 0
-
-		if len(haystack) >= len(needle):
-			for i in xrange(len(haystack)-len(needle)+1):
-				if haystack[i:i+len(needle)] == needle:
-					return i
-			return -1
-		else:
-			return -1
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if (not haystack) and (not needle):
+            return 0
+        
+        len_hay = len(haystack)
+        len_nee = len(needle)
+        
+        if len_hay >= len_nee:
+            for i in range(len_hay-len_nee+1):
+                if haystack[i:i+len_nee] == needle:
+                    return i
+                 
+        return -1
 
 
 t = Solution()
