52a0e4793e3f311bc2e22f4ad2a43b2e36c4a3f4
Evan Chang <chin.yeng@gmail.com>
2019-10-05 21:34:31 +0800

    Update 299_Bulls_and_Cows.py

--- algorithm/299_Bulls_and_Cows.py
+++ algorithm/299_Bulls_and_Cows.py
@@ -5,15 +5,14 @@ class Solution(object):
         :type guess: str
         :rtype: str
         """
-        bull = cow = 0
         s_dict = {}
         g_dict = {}
+        bulls = cows = 0
         
-        for i in xrange(len(secret)):
-            if secret[i] == guess[i]:
-                bull += 1
+        for i in range(len(secret)):
+            if secret[i] == guess[i]: 
+               bulls += 1
             else:
-                # use dict to calculate the value number.
                 if secret[i] not in s_dict:
                     s_dict[secret[i]] = 1
                 else:
@@ -22,10 +21,10 @@ class Solution(object):
                 if guess[i] not in g_dict:
                     g_dict[guess[i]] = 1
                 else:
-                    g_dict[guess[i]] += 1
+                    g_dict[guess[i]] += 1   
                     
-        for key in s_dict:
-            if key in g_dict:
-                cow += min(s_dict[key], g_dict[key])
+        for key in s_dict.keys():
+            if key in g_dict.keys():
+                cows += min(s_dict[key], g_dict[key])
                 
-        return "%sA%sB" %(bull, cow)
+        return "{}A{}B".format(bulls,cows)
