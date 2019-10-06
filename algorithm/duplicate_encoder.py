67512b86478c52e38dbc861a02849427fdb83cc8
Evan Chang <chin.yeng@gmail.com>
2019-10-06 21:45:12 +0800

    Create duplicate_encoder.py

---
+++ algorithm/duplicate_encoder.py
@@ -0,0 +1,22 @@
+def dup_encoder(s):
+    char_count = {}
+    s = s.lower()
+    for c in s:
+        if c not in char_count.keys():
+            char_count[c] = 1
+        else:
+            char_count[c] += 1
+
+    encode_str = ''
+    for c in s:
+        if char_count[c] == 1:
+            encode_str += '('
+        else:
+            encode_str += ')'
+
+    return encode_str
+
+print dup_encoder('din')
+print dup_encoder('recede')
+print dup_encoder('Success')
+print dup_encoder('(( @')d
