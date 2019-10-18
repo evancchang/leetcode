dc6ba8aaaf4017467e32ac59f0b60b4310a9f3fd
Evan Chang <chin.yeng@gmail.com>
2019-10-18 21:47:46 +0800

    Update 136_Single_Number.py

--- algorithm/136_Single_Number.py
+++ algorithm/136_Single_Number.py
@@ -1,15 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        h = {}
-        
-        for i in nums:
-            if i not in h.keys():
-                h[i] = 1
-            else:
-                h[i] += 1
-                
-        for k in h.keys():
-            if h[k] == 1:
-                return k
+        result = 0
+        for n in nums:
+            result ^= n
             
-        return -1
+        return result
