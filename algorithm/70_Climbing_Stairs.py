b9a3ee68bdc95f2e416f0be1c4b70475d6405162
Evan Chang <chin.yeng@gmail.com>
2019-12-29 21:06:11 +0800

    Update 70_Climbing_Stairs.py

--- algorithm/70_Climbing_Stairs.py
+++ algorithm/70_Climbing_Stairs.py
@@ -1,6 +1,21 @@
+# Time Limit Exceed
+#class Solution:
+#    def climbStairs(self, n: int) -> int:
+#        if n < 3:
+#            return n
+#        else:
+#            return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n < 3:
             return n
         else:
-            return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+            a = 1
+            b = 2
+            for _ in range(2, n):
+                c = a + b
+                a = b
+                b = c
+                
+            return c
