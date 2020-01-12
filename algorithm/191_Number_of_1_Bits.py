6f746359e6c43cc6c82561ab1ffafff05cf3c26f
Evan Chang <chin.yeng@gmail.com>
2020-01-12 10:32:39 +0800

    Update 191_Number_of_1_Bits.py

--- algorithm/191_Number_of_1_Bits.py
+++ algorithm/191_Number_of_1_Bits.py
@@ -1,10 +1,4 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        sn = bin(n).strip('0b')
-        out = 0
-        for i in range(len(sn)):
-            if sn[i] == '1':
-                out += 1
-                
-        return out
+        return bin(n).count('1')
         
