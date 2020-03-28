ed7a1e30093ff4c7bacdf1c6f19f60dba026134b
Evan Chang <chin.yeng@gmail.com>
2020-03-28 20:43:32 +0800

    Update 121_Best_Time_to_Buy_and_Sell_Stock.py

--- algorithm/121_Best_Time_to_Buy_and_Sell_Stock.py
+++ algorithm/121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -11,22 +11,22 @@
 #            
 #        return profit
 
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        if not prices:
-            return 0
-        
-        lowest_price = prices[0]
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
         max_profit = 0
-        for day in range(1, len(prices)):
-            if lowest_price > prices[day]:
-                lowest_price = prices[day]
+        min_price = float("inf")
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                max_profit = max(max_profit, price - min_price)
                 
-            if prices[day] - lowest_price > max_profit:
-                max_profit = prices[day] - lowest_price
-            
         return max_profit
-
+    
 t = Solution()
 #print t.maxProfit([7,1,5,3,6,4])
 print t.maxProfit([2,4,1])
