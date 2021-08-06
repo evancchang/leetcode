22939285764e2ea42d98a7a53644df688d66909e
Evan Chang <chin.yeng@gmail.com>
2021-08-06 20:32:58 +0800

    Update 151_Reverse_Words_in_a_String.py

--- algorithm/151_Reverse_Words_in_a_String.py
+++ algorithm/151_Reverse_Words_in_a_String.py
@@ -2,8 +2,5 @@ class Solution:
     def reverseWords(self, s: str) -> str:
         s_list = s.split()
         s_list.reverse()
-        new_s = ''
-        for ch in s_list:
-            new_s += ' ' + ch
 
-        return new_s.strip()        
+        return ' '.join(s_list)
