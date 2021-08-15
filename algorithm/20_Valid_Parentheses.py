ba5a74224b9aaee6fe9f39251baf766e6ccf3f2e
Evan Chang <chin.yeng@gmail.com>
2021-08-15 20:58:57 +0800

    Update 20_Valid_Parentheses.py

--- algorithm/20_Valid_Parentheses.py
+++ algorithm/20_Valid_Parentheses.py
@@ -1,18 +1,15 @@
-import collections
 class Solution:
     def isValid(self, s: str) -> bool:
-        ch_map = {')':'(', '}':'{', ']':'['}
-        stack = collections.deque()
-        
+        ch_map = {'(':')', '{':'}', '[':']'}
+        stack = []
         for ch in s:
-            if ch in ch_map.values():
+            if ch in ch_map.keys():
                 stack.append(ch)
             else:
                 if not stack:
                     return False
                 
-                if ch_map[ch] != stack.pop():
+                if ch_map[stack.pop()] != ch:
                     return False
                 
         return not stack
-                
