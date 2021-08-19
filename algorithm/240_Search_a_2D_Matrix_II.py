16d3877358ed35c88642d31041e20b55ea33a268
Evan Chang <chin.yeng@gmail.com>
2021-08-19 08:54:24 +0800

    Update 240_Search_a_2D_Matrix_II.py

--- algorithm/240_Search_a_2D_Matrix_II.py
+++ algorithm/240_Search_a_2D_Matrix_II.py
@@ -5,3 +5,27 @@ class Solution:
                 if n == target:
                     return True
         return False        
+
+class Solution2:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for m in matrix:
+            print(f'm = {m}')
+            if self.binarySearch(m, target):
+                return True
+        return False
+    
+    def binarySearch(self, l, item):
+        low = 0
+        high = len(l) - 1
+        
+        while low <= high:
+            mid = (low + high) // 2
+            guess = l[mid]
+            if guess == item:
+                return True
+            elif guess > item:
+                high = mid - 1
+            else:
+                low = mid + 1
+                
+        return False   
