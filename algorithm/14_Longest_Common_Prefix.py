452be72c2d675053798dca52f488deeaf9c37ec5
Evan Chang <chin.yeng@gmail.com>
2021-08-20 20:53:41 +0800

    Update 14_Longest_Common_Prefix.py

--- algorithm/14_Longest_Common_Prefix.py
+++ algorithm/14_Longest_Common_Prefix.py
@@ -17,6 +17,22 @@ class Solution(object):
                 
         return res
 
-t = Solution()
-#print t.longestCommonPrefix(['awbc', 'ab', 'aebcde'])
-print t.longestCommonPrefix(['a', 'a', 'b'])
+class Solution2:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs.sort(key=lambda x:len(x))
+        n = len(strs[0])
+        
+        ans = ''
+        need_break = False
+        for i in range(n):
+            curr_ch = strs[0][i]
+            for j in range(1, len(strs)):
+                if curr_ch != strs[j][i]:
+                    need_break = True
+                    break
+            if need_break:
+                break
+            
+            ans += curr_ch
+            
+        return ans
