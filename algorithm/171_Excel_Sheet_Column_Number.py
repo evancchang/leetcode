abe0e22c1b5ece679cb38787c069be2b33cda7ca
Evan Chang <chin.yeng@gmail.com>
2021-08-22 20:12:37 +0800

    Update 171_Excel_Sheet_Column_Number.py

--- algorithm/171_Excel_Sheet_Column_Number.py
+++ algorithm/171_Excel_Sheet_Column_Number.py
@@ -6,3 +6,16 @@ class Solution:
             ans += ord(ch) - 64
             
         return ans
+
+class Solution2:
+    def titleToNumber(self, columnTitle: str) -> int:
+        char_num = {'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H':8,
+                    'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15,
+                    'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22,
+                    'W':23, 'X':24, 'Y':25, 'Z':26}
+
+        n = len(columnTitle)
+        total = 0
+        for i in range(n-1, -1, -1):
+            total += char_num[columnTitle[-i-1]] * (26 ** i)
+        return total
