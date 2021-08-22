0c8057161e82356494b16bf2d76ece02e588ca1f
Evan Chang <chin.yeng@gmail.com>
2021-08-22 09:44:10 +0800

    Update 136_Single_Number.py

--- algorithm/136_Single_Number.py
+++ algorithm/136_Single_Number.py
@@ -2,6 +2,20 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         result = 0
         for n in nums:
-            result ^= n
+            result ^= n # use XOR
             
         return result
+
+class Solution2:
+    def singleNumber(self, nums: List[int]) -> int:
+        num_counts = {}
+        
+        for num in nums:
+            if num not in num_counts:
+                num_counts[num] = 1
+            else:
+                num_counts[num] += 1
+                
+        for k, v in num_counts.items():
+            if v == 1:
+                return k
