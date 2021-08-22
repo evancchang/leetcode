813875e7686df18461cbfe446f24f0f3400ebbfb
Evan Chang <chin.yeng@gmail.com>
2021-08-22 09:56:02 +0800

    Update 169_Majority_Element.py

--- algorithm/169_Majority_Element.py
+++ algorithm/169_Majority_Element.py
@@ -3,3 +3,25 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         c = Counter(nums)
         return c.most_common(1)[0][0]
+
+class Solution2:
+    def majorityElement(self, nums: List[int]) -> int:
+        num_counts = {}
+        major_count = 0
+        major_element = 0
+        
+        for num in nums:
+            if num not in num_counts:
+                num_counts[num] = 1
+            else:
+                num_counts[num] += 1
+                
+        times = len(nums) // 2
+        
+        for k, v in num_counts.items():
+            if v > times:
+                if v > major_count:
+                    major_count = v
+                    major_element = k
+                
+        return major_element
