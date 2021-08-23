a226873d6189b5adbe449f077afc3a444c200ccc
Evan Chang <chin.yeng@gmail.com>
2021-08-23 08:30:56 +0800

    Update 204_Count_Primes.py

--- algorithm/204_Count_Primes.py
+++ algorithm/204_Count_Primes.py
@@ -1,22 +1,19 @@
-"""
-import math
 class Solution:
     def countPrimes(self, n: int) -> int:
         count = 0
+        
         for i in range(2, n):
-            count += self.is_Primes(i)
-            
-        return count    
+            count += self.isPrime(i)
         
-        
-    def is_Primes(self, n: int) -> int:
-        for i in range(2, int(math.sqrt(n))+1):
+        return count
+    
+    def isPrime(self, n):
+        for i in range(2, int(n**(1/2))+1):
             if n % i == 0:
                 return 0
         return 1
-"""        
                 
-class Solution:
+class Solution2:
     def countPrimes(self, n: int) -> int:
         if n == 0:
             prime = [False]
