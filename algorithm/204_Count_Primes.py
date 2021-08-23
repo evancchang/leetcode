7899e015e3987ff1fb4d370a277e9ca8b543f3e1
Evan Chang <chin.yeng@gmail.com>
2021-08-23 08:32:55 +0800

    Update 204_Count_Primes.py

--- algorithm/204_Count_Primes.py
+++ algorithm/204_Count_Primes.py
@@ -8,7 +8,7 @@ class Solution:
         return count
     
     def isPrime(self, n):
-        for i in range(2, int(n**(1/2))+1):
+        for i in range(2, int(n**0.5)+1):
             if n % i == 0:
                 return 0
         return 1
