2055a1f354f601b381360af009cb96aef8f659d1
Evan Chang <chin.yeng@gmail.com>
2021-08-29 10:19:51 +0800

    Update 905_Sort_Array_By_Parity.py

--- algorithm/905_Sort_Array_By_Parity.py
+++ algorithm/905_Sort_Array_By_Parity.py
@@ -9,3 +9,13 @@ class Solution:
                 odd.append(n)
                 
         return even + odd
+
+class Solution2:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        slow = 0
+        for fast in range(len(nums)): # use slow pointer to record the odd value idx
+            if nums[fast] % 2 == 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
+                
+        return nums        
