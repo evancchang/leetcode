1c2791fc5459c5846467dd8c4705cd27bdce90f4
Evan Chang <chin.yeng@gmail.com>
2021-08-30 08:33:41 +0800

    Update 700_Search_in_a_Binary_Search_Tree.py

--- algorithm/700_Search_in_a_Binary_Search_Tree.py
+++ algorithm/700_Search_in_a_Binary_Search_Tree.py
@@ -22,3 +22,26 @@ class Solution(object):
         else:
             return self.searchBST(root.left, val)
         
+class Solution2(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        
+        q = [root]
+        while q:
+            curr = q.pop(0)
+            if curr.val == val:
+                return curr
+            elif curr.val < val:
+                if curr.right:
+                    q.append(curr.right)
+            else:
+                if curr.left:
+                    q.append(curr.left)
+                
+        return None
