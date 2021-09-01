632e9b729f741a5c6cf73701fa8918e108f20f5a
Evan Chang <chin.yeng@gmail.com>
2021-09-01 08:06:49 +0800

    Update 162_Find_Peak_Element.py

--- algorithm/162_Find_Peak_Element.py
+++ algorithm/162_Find_Peak_Element.py
@@ -7,3 +7,16 @@ class Solution:
             
         return n-1
         
+class Solution2:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[mid+1]:
+                right = mid
+            else:
+                left = mid + 1
+                
+        return left
