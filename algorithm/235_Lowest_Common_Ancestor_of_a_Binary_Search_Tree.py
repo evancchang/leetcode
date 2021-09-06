ef4524f62df73d80838ae776fcb42b845067b31c
Evan Chang <chin.yeng@gmail.com>
2021-09-06 19:23:39 +0800

    Update 235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py

--- algorithm/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
+++ algorithm/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -7,18 +7,10 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root or root == p or root == q:
-            return root
-        else:
-            left = self.lowestCommonAncestor(root.left, p, q)
-            right = self.lowestCommonAncestor(root.right, p, q)
-            
-        if left and right:
-            return root
-        elif left:
-            return left
-        elif right:
-            return right
-        else:
-            return None
-        
+        while root:
+            if (root.val > p.val) and (root.val > q.val):
+                root = root.left
+            elif (root.val < p.val) and (root.val < q.val):
+                root = root.right
+            else:
+                return root
