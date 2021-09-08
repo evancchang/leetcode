6dbc9e898cc191d1bed32e5ffb7029190a459d93
Evan Chang <chin.yeng@gmail.com>
2021-09-08 08:12:49 +0800

    Update 349_Intersection_of_Two_Arrays.py

--- algorithm/349_Intersection_of_Two_Arrays.py
+++ algorithm/349_Intersection_of_Two_Arrays.py
@@ -7,3 +7,10 @@ class Solution:
                     ans.append(num1)
                 
         return ans        
+
+class Solution2:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+        
+        return list(set1&set2)
