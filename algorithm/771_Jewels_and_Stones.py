145d562bf7de9b3c5b30fde4e592519bee067ffd
Evan Chang <chin.yeng@gmail.com>
2021-09-09 13:59:09 +0800

    Update 771_Jewels_and_Stones.py

--- algorithm/771_Jewels_and_Stones.py
+++ algorithm/771_Jewels_and_Stones.py
@@ -1,11 +1,7 @@
 class Solution:
-    def numJewelsInStones(self, J: str, S: str) -> int:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
         ans = 0
-        for ch in J:
-            for c in S:
-                if ch == c:
-                    S.replace(ch, '', 1)
-                    ans += 1
-                
+        for ch in stones:
+            if ch in jewels:
+                ans += 1
         return ans
-        
