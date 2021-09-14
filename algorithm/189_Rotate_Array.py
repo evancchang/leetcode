50b0e41ec978a6019a389dae70f5684cb4ea5c4e
Evan Chang <chin.yeng@gmail.com>
2021-09-14 08:41:39 +0800

    Update 189_Rotate_Array.py

--- algorithm/189_Rotate_Array.py
+++ algorithm/189_Rotate_Array.py
@@ -1,16 +1,8 @@
-class Solution(object):
-    def rotate(self, nums, k):
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
         """
-        :type nums: List[int]
-        :type k: int
-        :rtype: void Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        k = k % n
+        k = k % len(nums) # if k > len(nums), we can find the same k steps
         nums[:] = nums[-k:] + nums[:-k]
-        
-
-t = Solution()
-#print t.rotate([1,2,3,4,5,6,7], 3)
-#print t.rotate([1,2,3,4,5,6,7], 0)
-print t.rotate([1,2], 1)
+        return nums
