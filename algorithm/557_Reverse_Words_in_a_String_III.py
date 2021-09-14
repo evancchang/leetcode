9c56a47fdc563ba412abf36e89461bb2bc4e0617
Evan Chang <chin.yeng@gmail.com>
2021-09-14 19:58:14 +0800

    Update 557_Reverse_Words_in_a_String_III.py

--- algorithm/557_Reverse_Words_in_a_String_III.py
+++ algorithm/557_Reverse_Words_in_a_String_III.py
@@ -4,3 +4,12 @@ class Solution:
         s = s.split()
         s = s[::-1]
         return ' '.join(s)
+
+class Solution2:
+    def reverseWords(self, s: str) -> str:
+        s_list = s.split()
+        ans = ''
+        for s in s_list:
+            ans += s[::-1] + ' '
+            
+        return ans.strip()
