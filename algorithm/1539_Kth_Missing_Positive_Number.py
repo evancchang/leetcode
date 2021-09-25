bcc17e75f798dd52f58f2190145585e2da5f1ab1
Evan Chang <chin.yeng@gmail.com>
2021-09-25 20:38:11 +0800

    Create 1539_Kth_Missing_Positive_Number.py

---
+++ algorithm/1539_Kth_Missing_Positive_Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        miss_count = 0
+        max_val = arr[-1]
+        
+        for i in range(1, max_val+1):
+            if i not in arr:
+                miss_count += 1
+                if miss_count == k:
+                    return i
+                
+        remain = k - miss_count
+        return arr[-1] + remain
