2a97042a2bda82d05e966968c32f191061759c57
Evan Chang <chin.yeng@gmail.com>
2021-09-30 16:04:38 +0800

    Update 53_Maximum_Subarray.py

--- algorithm/53_Maximum_Subarray.py
+++ algorithm/53_Maximum_Subarray.py
@@ -14,3 +14,16 @@ class Solution:
             total = max(total, curr)
                 
         return total
+
+class Solution2:
+    def maxSubArray(self, nums: List[int]) -> int:
+        ans = float('-inf')
+        accumulate = 0
+        
+        for num in nums:
+            accumulate += num
+            ans = max(ans, accumulate)
+            if accumulate < 0:
+                accumulate = 0
+                
+        return ans
