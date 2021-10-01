f24ee018bb38d6376932a2d56f011d79a7330ee8
Evan Chang <chin.yeng@gmail.com>
2021-10-01 08:21:47 +0800

    Update 350_Intersection_of_Two_Arrays_II.py

--- algorithm/350_Intersection_of_Two_Arrays_II.py
+++ algorithm/350_Intersection_of_Two_Arrays_II.py
@@ -1,6 +1,6 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans = []
+        intersection = []
         nums1_count = {}
         
         for num in nums1:
@@ -10,11 +10,8 @@ class Solution:
                 nums1_count[num] += 1
                 
         for num in nums2:
-            if num in nums1_count:
-                if nums1_count[num] > 0:
-                    ans.append(num)
-                    nums1_count[num] -= 1
-                    
-        return ans
-        
-        
+            if (num in nums1) and (nums1_count[num] > 0):
+                nums1_count[num] -= 1
+                intersection.append(num)
+                
+        return intersection
