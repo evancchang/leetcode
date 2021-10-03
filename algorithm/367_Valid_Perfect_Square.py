46d2f3a002aa882f31d92e975556d29f3b9f3b96
Evan Chang <chin.yeng@gmail.com>
2021-10-03 10:21:08 +0800

    Update 367_Valid_Perfect_Square.py

--- algorithm/367_Valid_Perfect_Square.py
+++ algorithm/367_Valid_Perfect_Square.py
@@ -4,3 +4,21 @@ class Solution:
             if i * i == num:
                 return True
         return False
+
+class Solution2:
+    def isPerfectSquare(self, num: int) -> bool:
+        left = 0
+        right = int(num**0.5) + 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            val = mid * mid
+            
+            if val == num:
+                return True
+            elif val > num:
+                right = mid - 1
+            else:
+                left = mid + 1
+                
+        return False    
