fde622bf78a76b7dd5948ad821f3e0c55313e567
Evan Chang <chin.yeng@gmail.com>
2021-10-07 16:22:48 +0800

    Update 705_Design_HashSet.py

--- algorithm/705_Design_HashSet.py
+++ algorithm/705_Design_HashSet.py
@@ -24,6 +24,23 @@ class MyHashSet:
         
 
 
+class MyHashSet2:
+
+    def __init__(self):
+        self.list = []
+
+    def add(self, key: int) -> None:
+        if key not in self.list:
+            self.list.append(key)
+
+    def remove(self, key: int) -> None:
+        if key in self.list:
+            self.list.remove(key)
+
+    def contains(self, key: int) -> bool:
+        return key in self.list        
+
+
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
 # obj.add(key)
