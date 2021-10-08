29ad6707c1a1bb645f32f4b3d1eb3e8cb74ce287
Evan Chang <chin.yeng@gmail.com>
2021-10-08 16:45:46 +0800

    Update 49_Group_Anagrams.py

--- algorithm/49_Group_Anagrams.py
+++ algorithm/49_Group_Anagrams.py
@@ -7,3 +7,19 @@ class Solution:
                 ans_dict[tuple(sorted(s))].append(s)
                 
         return ans_dict.values()
+
+class Solution2:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = {}
+        ans = []
+        for str in strs:
+            if tuple(sorted(str)) not in anagram_map:
+                anagram_map[tuple(sorted(str))] = [str]
+            else:
+                anagram_map[tuple(sorted(str))].append(str)
+                
+        for k, v in anagram_map.items():
+            ans.append(v)
+            
+        return ans
+        
