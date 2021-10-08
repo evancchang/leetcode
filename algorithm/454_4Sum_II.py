3f21a720e0d82ee15bb01fa483255e3e6e70cc5e
Evan Chang <chin.yeng@gmail.com>
2021-10-08 20:01:41 +0800

    Update 454_4Sum_II.py

--- algorithm/454_4Sum_II.py
+++ algorithm/454_4Sum_II.py
@@ -13,3 +13,23 @@ class Solution:
                 ans += result[-(k+l)]
                     
         return ans
+    
+class Solution2:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        tmp_sum = {}
+        ans = 0
+        
+        for i in nums1:
+            for j in nums2:
+                tmp = i + j
+                if tmp not in tmp_sum:
+                    tmp_sum[tmp] = 1
+                else:
+                    tmp_sum[tmp] += 1
+                    
+        for k in nums3:
+            for l in nums4:
+                tmp = k + l
+                ans += tmp_sum.get(-tmp, 0)
+                
+        return ans
