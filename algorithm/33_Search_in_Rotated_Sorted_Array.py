f703e3edb71a310b38755b2e0d9f9bf3a8dd5c6c
Evan Chang <chin.yeng@gmail.com>
2022-11-24 20:52:57 +0800

    Update 33_Search_in_Rotated_Sorted_Array.py

--- algorithm/33_Search_in_Rotated_Sorted_Array.py
+++ algorithm/33_Search_in_Rotated_Sorted_Array.py
@@ -6,17 +6,16 @@ class Solution:
         while left <= right:
             mid = (left + right) // 2
             guess = nums[mid]
-            if guess == target:
+            if target == guess:
                 return mid
             elif nums[left] <= guess:
-                if target < guess and target >= nums[left]:
+                if nums[left] <= target <= guess:
                     right = mid - 1
                 else:
-                    left = mid + 1
+                    left = mid + 1 
             else:
-                if target > guess and target <= nums[right]:
+                if guess <= target <= nums[right]:
                     left = mid + 1
                 else:
-                    right = mid - 1
-                
+                    right = mid -1
         return -1
