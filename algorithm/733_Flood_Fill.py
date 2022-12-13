dfa79b5be4c7cf4deee803433a73204087085050
Evan Chang <chin.yeng@gmail.com>
2022-12-13 09:38:00 +0800

    Update 733_Flood_Fill.py

--- algorithm/733_Flood_Fill.py
+++ algorithm/733_Flood_Fill.py
@@ -17,3 +17,22 @@ class Solution:
         
         dfs(sr, sc)
         return image
+
+class Solution2:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        curr_color = image[sr][sc]
+        if curr_color == newColor:
+            return image
+
+        self.dfs(image, sr, sc, curr_color, newColor)
+        return image
+
+    def dfs(self, grid, x, y, curr_color, newColor):
+        if x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]) or grid[x][y] != curr_color:
+            return
+
+        grid[x][y] = newColor
+        self.dfs(grid, x+1, y, curr_color, newColor)
+        self.dfs(grid, x-1, y, curr_color, newColor)
+        self.dfs(grid, x, y+1, curr_color, newColor)
+        self.dfs(grid, x, y-1, curr_color, newColor)
