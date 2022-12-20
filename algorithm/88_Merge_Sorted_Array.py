19cf85887fa29bc7dfe926ef6ad2401944bac825
Evan Chang <chin.yeng@gmail.com>
2022-12-20 17:02:37 +0800

    Update 88_Merge_Sorted_Array.py

--- algorithm/88_Merge_Sorted_Array.py
+++ algorithm/88_Merge_Sorted_Array.py
@@ -1,12 +1,16 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: void Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
-        nums1[m:] = nums2[:n]
-        nums1.sort()  
-        
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+
+        if n > 0:
+            for i in range(n):
+                nums1[i] = nums2[i]
