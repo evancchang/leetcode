e48c66af518713070ddc7abe5a1acdd62040a086
Evan Chang <chin.yeng@gmail.com>
2022-12-25 17:31:55 +0800

    Update 739_Daily_Temperatures.py

--- algorithm/739_Daily_Temperatures.py
+++ algorithm/739_Daily_Temperatures.py
@@ -10,16 +10,3 @@ class Solution:
             stack.append(i)
         return ans
 
-class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        ans = [0] * len(temperatures)
-
-        for i, temp in enumerate(temperatures):
-            day_pass = 0
-            for j in range(i+1, len(temperatures)):
-                if temperatures[j] > temp:
-                    ans[i] = day_pass + 1
-                    break
-                else:
-                    day_pass += 1           
-        return ans
